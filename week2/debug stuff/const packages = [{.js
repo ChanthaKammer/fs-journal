1c1cf958157a454c7177a6faa8d887a75e0fa9dc
ChanthaKammer <93282397+ChanthaKammer@users.noreply.github.com>
2023-05-03 21:22:35 -0600

    Create const packages = [{.js

---
+++ week2/debug stuff/const packages = [{.js
@@ -0,0 +1,211 @@
+const packages = [{
+      priorityLevel: 'express',
+      isFragile: false,
+      weight: 2,
+      to: 'Sir Harrington IV',
+      trackingNumber: '1324kjs'
+   },
+   {
+      priorityLevel: 'standard',
+      isFragile: true,
+      weight: .5,
+      to: 'Master Mercutio',
+      trackingNumber: '1325sdk'
+   },
+   {
+      priorityLevel: 'free',
+      isFragile: true,
+      weight: .5,
+      to: 'Mistress Ravenfeather',
+      trackingNumber: 'jffd147'
+   },
+   {
+      priorityLevel: 'standard',
+      isFragile: false,
+      weight: 4,
+      to: 'B. Robert Brown',
+      trackingNumber: 'acdc145'
+   },
+   {
+      priorityLevel: 'express',
+      isFragile: true,
+      weight: 6,
+      to: 'Chancellor Wallace',
+      trackingNumber: '4b2l0z'
+   },
+   {
+      priorityLevel: 'standard',
+      isFragile: false,
+      weight: 5,
+      to: 'Sarah Sharm',
+      trackingNumber: '8081baz'
+   },
+   {
+      priorityLevel: 'free',
+      isFragile: true,
+      weight: 12,
+      to: 'Tae Lien',
+      trackingNumber: 'suz2367'
+   },
+   {
+      priorityLevel: 'express',
+      isFragile: true,
+      weight: 9,
+      to: 'Ada Lovelace',
+      trackingNumber: 'u4t5s6r7'
+   },
+   {
+      priorityLevel: 'standard',
+      isFragile: false,
+      weight: 1,
+      to: 'Alan Turing',
+      trackingNumber: 'v5u4t3s2'
+   },
+   {
+      priorityLevel: 'free',
+      isFragile: true,
+      weight: 2.2,
+      to: 'Grace Hopper',
+      trackingNumber: 'w6v5u4t3'
+   },
+   {
+      priorityLevel: 'express',
+      isFragile: false,
+      weight: 4.7,
+      to: 'Tim Berners-Lee',
+      trackingNumber: 'x7w6v5u4'
+   },
+   {
+      priorityLevel: 'standard',
+      isFragile: true,
+      weight: 3.1,
+      to: 'Linus Torvalds',
+      trackingNumber: 'y8x7w6v5'
+   },
+   {
+      priorityLevel: 'free',
+      isFragile: false,
+      weight: 4.4,
+      to: 'Bjarne Stroustrup',
+      trackingNumber: 'z9y8x7w6'
+   },
+   {
+      priorityLevel: 'express',
+      isFragile: true,
+      weight: 3.3,
+      to: 'Guido van Rossum',
+      trackingNumber: 'a1z2y3x4'
+   },
+   {
+      priorityLevel: 'standard',
+      isFragile: false,
+      weight: 1.6,
+      to: 'James Gosling',
+      trackingNumber: 'b2a3z4y5'
+   },
+   {
+      priorityLevel: 'free',
+      isFragile: true,
+      weight: 0.8,
+      to: 'Larry Page',
+      trackingNumber: 'c3b2a1z0'
+   },
+   {
+      priorityLevel: 'express',
+      isFragile: false,
+      weight: 5.5,
+      to: 'Sergey Brin',
+      trackingNumber: 'd4c3b2a1'
+   },
+   {
+      priorityLevel: 'standard',
+      isFragile: true,
+      weight: 1.9,
+      to: 'Elon Musk',
+      trackingNumber: 'e5d4c3b2'
+   },
+   {
+      priorityLevel: 'free',
+      isFragile: false,
+      weight: 3.8,
+      to: 'Jeff Bezos',
+      trackingNumber: 'f6e5d4c3'
+   },
+   {
+   priorityLevel: 'express',
+   isFragile: true,
+   weight: 7.1,
+   to: 'Bill Gates',
+   trackingNumber: 'g7f6e5d4'
+   },
+   {
+   priorityLevel: 'standard',
+   isFragile: false,
+   weight: 3.6,
+   to: 'Mark Zuckerberg',
+   trackingNumber: 'h8g7f6e5'
+   },
+   {
+   priorityLevel: 'free',
+   isFragile: true,
+   weight: 5.2,
+   to: 'Sheryl Sandberg',
+   trackingNumber: 'i9h8g7f6'
+   },
+   {
+   priorityLevel: 'express',
+   isFragile: false,
+   weight: 6.3,
+   to: 'Satya Nadella',
+   trackingNumber: 'j0i9h8g7'
+   },
+   {
+   priorityLevel: 'standard',
+   isFragile: true,
+   weight: 4.9,
+   to: 'Sundar Pichai',
+   trackingNumber: 'k1j0i9h8'
+   },
+   {
+   priorityLevel: 'free',
+   isFragile: false,
+   weight: 2.7,
+   to: 'Marissa Mayer',
+   trackingNumber: 'l2k1j0i9'
+   },
+   {
+   priorityLevel: 'express',
+   isFragile: true,
+   weight: 10.5,
+   to: 'Susan Wojcicki',
+   trackingNumber: 'm3l2k1j0'
+   },
+   {
+   priorityLevel: 'standard',
+   isFragile: false,
+   weight: 8.8,
+   to: 'Jack Dorsey',
+   trackingNumber: 'n4m3l2k1'
+   },
+   {
+   priorityLevel: 'free',
+   isFragile: true,
+   weight: 1.1,
+   to: 'Steve Jobs',
+   trackingNumber: 'o5n4m3l2'
+   },
+   {
+   priorityLevel: 'express',
+   isFragile: false,
+   weight: 2.6,
+   to: 'Stephen Hawking',
+   trackingNumber: 'p6o5n4m3'
+   },
+   {
+   priorityLevel: 'standard',
+   isFragile: true,
+   weight: 7.2,
+   to: 'Chantha',
+   trackingNumber: '( ͡° ͜ʖ ͡°)'
+   }
+   ];
